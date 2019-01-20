670393694eb25f229f70d722deee35c87f502f1d
condac <burns.nu@gmail.com>
2019-01-20 13:15:59 +0100

    Power functions

--- PowerMeter/PowerMeter.ino
+++ PowerMeter/PowerMeter.ino
@@ -1,23 +1,32 @@
 #include <avr/sleep.h>
 
 #include <RH_ASK.h>
-RH_ASK rf_driver;
 
-#define TX_POWER 10 // Use 2 power pins to spread the load
-#define TX_POWER2 11 // the tx use 9-40mA and pins can only deliver 20mA
 
-#define TX_OUT 12 // This cant be changed, hardcoded in RH_ASK driver
+
+#define VCC_PIN 6 // power for radio
+#define GND_PIN 5
+
+#define RX_PIN 5 // not used
+#define TX_PIN 7
 #define LED_PIN LED_BUILTIN
 
-
+#define TX_TIMER 10000 // how often we send information 
 
 #define BLINK_PIN 2
 
-#define DEVICE_ID 123 // Uniqe id in your network
+#define DEVICE_ID 3 // Uniqe id in your network
+
+
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver(1000,RX_PIN,TX_PIN);
+
 
 long blinkCounter = 0;
 long lastCounter = 0;
 long lastTime = -1000000000;
+long txDelay = 0;
+long battV;
 
 float watts = 0.0;
 
@@ -26,10 +35,7 @@ void setup() {
   Serial.println("Setup");
 
 
-  pinMode (TX_POWER, INPUT); // Power pin is left as INPUT to have it floating when not used
-  digitalWrite (TX_POWER, LOW); // If it is set to OUTPUT it will shortout when one of the pins go HIGH
-  pinMode (TX_POWER2, INPUT);
-  digitalWrite (TX_POWER2, LOW);
+  powerDown();
 
   pinMode (BLINK_PIN, INPUT);
   digitalWrite (BLINK_PIN, HIGH); 
@@ -37,6 +43,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   
   rf_driver.init();
+  randomSeed(analogRead(4));
 
   attachInterrupt(digitalPinToInterrupt(BLINK_PIN), interuptFunction, FALLING);
 }
@@ -48,69 +55,26 @@ void loop() {
     lastCounter = blinkCounter;
     loopHandle();
   }
-
+  if (txDelay<millis()) {
+    txDelay = TX_TIMER+millis()+random(3000);
+    sendRadio();
+  }
 }
 
 void loopHandle() {
-  Serial.println("loopHandle");
-  // Main function when we see a blink on the power meter
-  // Steps to do:
-  // 1. Power on transmitter, so it can warm up while we do other stuff
-  // 2. Calculate time since last blink
-  // 3. Add to counter for number of blinks seen
-  // 4. Measure battery voltage
-  // 5. Send all data
-  // 6. Power off
-
-  
-  // 1. Power on transmitter
-  pinMode (TX_POWER, OUTPUT); 
-  pinMode (TX_POWER2, OUTPUT);
-  digitalWrite (TX_POWER, LOW); // GND of TX is connected to pins so pulling it LOW power the TX on
-  digitalWrite (TX_POWER2, LOW); 
-
-  pinMode (LED_PIN, OUTPUT);
-  digitalWrite (LED_PIN, LOW);
+  //Serial.println("loopHandle");
 
   // 2. Calculate time since last blink
   long currentTime = millis();
   long deltaTime = currentTime - lastTime;
   lastTime = currentTime;
   watts = 3600000.0/deltaTime; //TODO
-  Serial.println(deltaTime);
-  Serial.println(millis());
+
   // 3. Add to counter for number of blinks seen
   // moved to interupt function
   
-
-  
   // 4. Measure battery voltage
-  long battV = vccVoltage();
-  
-  // 5. Send all data
-  String out = "{\"id\":";
-  out += DEVICE_ID;
-  out += ",\"b\":";
-  out.concat(blinkCounter);
-  out += ",\"w\":";
-  out.concat(watts);
-  out += ",\"v\":";
-  out.concat(battV);
-  
-  out += ",}"; // end
-
-  // TODO send multiple times?
-  Serial.println(out);
-  static char *msg = out.c_str();
-    
-  rf_driver.send((uint8_t *)msg, strlen(msg));
-  rf_driver.waitPacketSent();
-
-  // 6. Power off
-  pinMode (TX_POWER, INPUT); 
-  pinMode (TX_POWER2, INPUT);
-  digitalWrite (TX_POWER, LOW); // GND of TX is connected to pins so pulling it LOW power the TX on
-  digitalWrite (TX_POWER2, LOW); 
+  battV = vccVoltage();
   
   digitalWrite (LED_PIN, HIGH);
   pinMode (LED_PIN, OUTPUT);
@@ -131,4 +95,49 @@ long vccVoltage() {
   result |= ADCH<<8;
   result = 1126400L / result; // Back-calculate AVcc in mV
   return result;
+}
+
+void sendRadio() {
+  powerOn();
+  // 5. Send all data
+  String out = "{\"id\":";
+  out += DEVICE_ID;
+  out += ",\"b\":";
+  out.concat(blinkCounter);
+  out += ",\"w\":";
+  out.concat(watts);
+  out += ",\"v\":";
+  out.concat(battV);
+  
+  out += ",}"; // end
+
+  // TODO send multiple times?
+  Serial.println(out);
+  static char *msg = out.c_str();
+    
+  rf_driver.send((uint8_t *)msg, strlen(msg));
+  rf_driver.waitPacketSent();
+
+  // 6. Power off
+  powerDown();
+}
+void powerDown() {
+  pinMode (GND_PIN, OUTPUT);
+  digitalWrite (GND_PIN, LOW); 
+  pinMode (VCC_PIN, OUTPUT);
+  digitalWrite (VCC_PIN, LOW);   
+  
+  pinMode (LED_PIN, OUTPUT);
+  digitalWrite (LED_PIN, LOW); 
+}
+
+void powerOn() {
+  pinMode (GND_PIN, OUTPUT);
+  digitalWrite (GND_PIN, LOW); 
+  pinMode (VCC_PIN, OUTPUT);
+  digitalWrite (VCC_PIN, HIGH);   
+  
+  pinMode (LED_PIN, OUTPUT);
+  digitalWrite (LED_PIN, HIGH); 
+  
 }
